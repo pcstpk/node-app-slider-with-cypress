0776011be133156de3ef4132ac7ee6a534c58dc8
pcstpk <202306725+pcstpk@users.noreply.github.com>
2025-03-24 23:43:18 +0100

    Add test and hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,20 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Navigates forward and backward through slides', function () {
+    cy.visit('http://localhost:3000');
+    
+    cy.get('.swiper-slide-active').invoke('text').then((firstSlideText) => {
+      
+      cy.get('.swiper-button-next').click();
+      cy.wait(2000);
+      cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+      
+      cy.get('.swiper-button-prev').click();
+      cy.wait(2000);
+      cy.get('.swiper-slide-active').should('contain', firstSlideText);
+    });
+  });
+});
